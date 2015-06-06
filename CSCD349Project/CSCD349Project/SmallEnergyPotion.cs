6798af9f868dd01bb4f10a08ac7810b3d5402516
iamaudrey <greataudrey@gmail.com>
2015-06-06 16:16:05 -0700

    Changed console output to Messaging output
    changed console output to Messaging output

--- CSCD349Project/CSCD349Project/SmallEnergyPotion.cs
+++ CSCD349Project/CSCD349Project/SmallEnergyPotion.cs
@@ -17,7 +17,7 @@ namespace CSCD349Project
         public override void Use(GameCharacter character)
         {
             character.GetAttributes()._energy += 20;
-            Console.WriteLine("The potion restored 20 energy points!");
+            AddMessage("The potion restored 20 energy points!");
         }
     }
 }
